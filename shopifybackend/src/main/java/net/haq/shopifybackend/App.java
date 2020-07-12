d9df8f517942f74b810a2eab79d247a9c7c6c2a4
hujjajali <hujjajali@gmail.com>
2020-07-12 16:59:42 +0500

    Project setup Commit for E-Commerce Application

---
+++ shopifybackend/src/main/java/net/haq/shopifybackend/App.java
@@ -0,0 +1,13 @@
+package net.haq.shopifybackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
